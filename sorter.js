b181cb9d29df037b6ae7fdbd332d042572add39b
viksuper555 <viksuper555@gmail.com>
2019-03-19 08:15:30 +0200

    Removed background animation functionality, added car divs

--- sorter.js
+++ sorter.js
@@ -1,14 +1,15 @@
     function sortListAsc(){
-      b = document.getElementById("id01").getElementsByTagName("LI");
-      arr = [];
+      a = document.getElementById("id01").getElementsByTagName("LI");
+      b = document.getElementById("id01").getElementsByTagName("h5");
+      arr = [], arr2 = [];
 
       for (let index = 0; index < b.length; index++) {
-        arr[index] = b[index].innerHTML;
+        arr[index] = b[index].innerHTML.substr(0,b[index].innerHTML.length-2);
+        arr2[index] = a[index].innerHTML;
       }
-      arr.sort();
+
       for (let index = 0; index < b.length; index++) {
-        b[index].innerHTML = arr[index];
-        
+        a[index].innerHTML = arr2[index];        
       }
     }
     function sortListDesc(){
@@ -25,4 +26,5 @@
         
       }
     }
+ 
 
