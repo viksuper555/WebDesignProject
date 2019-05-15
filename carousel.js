85a64f2239a640dec4e04617688b829d937b9193
viksuper555 <viksuper555@gmai.com>
2019-05-15 21:28:45 +0300

    Worked on homepage and some tweaks

--- carousel.js
+++ carousel.js
@@ -1,11 +1,11 @@
 var owl = $('.owl-carousel');
 owl.owlCarousel({
-    items:4,
+    items:2,
     loop:true,
     margin:10,
     autoplay:true,
     autoplayTimeout:3000,
-    autoplayHoverPause:true
+    autoplayHoverPause:false
 });
 $('.play').on('click',function(){
     owl.trigger('play.owl.autoplay',[1000])
