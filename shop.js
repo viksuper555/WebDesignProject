0a6b53e5667d88873c0faea9b3e7191111f6dbcc
viksuper555 <viksuper555@gmai.com>
2019-05-16 19:31:11 +0300

    Preferred version for revisions.
    Refactored the code, files and functionality at its finest.

--- shop.js
+++ shop.js
@@ -1,72 +1,276 @@
+//
+// Updates cars for sale for each category and the randomized main page.
 
 var luxury = document.getElementById("luxury");       
 var economy = document.getElementById("economy");
 var cheap = document.getElementById("cheap"); 
-
-    var luxuryDescriptions = ["Mercedes Benz S Class", "Range Rover", "Jaguar I-Pace", "Tesla Model S", "Audi A8", "BMW 7 Series", "Jaguar XJ",
-    "BMW 6 Series Gran Turismo", "Lexus LS", "Maserati Quattroporte", "Porche Panamera", "Lamborghini Aventador"];
-    var luxuryTitles = ["Mercedes Benz S Class", "Range Rover", "Jaguar I-Pace", "Tesla Model S", "Audi A8", "BMW 7 Series", "Jaguar XJ",
-    "BMW 6 Series Gran Turismo", "Lexus LS", "Maserati Quattroporte", "Porche Panamera", "Lamborghini Aventador"];
-    var luxuryPrices = ["200.000лв", "200.000лв", "200.000лв", "200.000лв", "200.000лв", "200.000лв", "200.000лв", "200.000лв", "200.000лв", "200.000лв", "200.000лв", "200.000лв"];
-    var economyDescriptions = ["Mini Cooper", "Honda Civic Hatchback", "Honda Accord", "Volkswagen Up!", "Ford Fiesta", "Kia Soul", "Chevrolet Spark",
-    "Chevrolet Sonic", "Volkswagen Golf", "Hyundai Veloster", "Fiat 500", "BMW i3"];
-    var economyTitles = ["Mini Cooper", "Honda Civic Hatchback", "Honda Accord", "Volkswagen Up!", "Ford Fiesta", "Kia Soul", "Chevrolet Spark",
-    "Chevrolet Sonic", "Volkswagen Golf", "Hyundai Veloster", "Fiat 500", "BMW i3"];
-    var economyPrices = ["20.000лв", "20.000лв", "20.000лв", "20.000лв", "20.000лв", "20.000лв", "20.000лв", "20.000лв", "20.000лв", "20.000лв", "20.000лв", "20.000лв"];
-    var cheapDescriptions = ["Golf 3", "Volkswagen Polo", "Ford Escort", "Renault Megan", "Renault Clio",
-    "Moskwitsch", "Citroen C2", "Audi A3", "BMW M3", "Peugeot 203", "Opel Astra", "Mercedes 190"];
-    var cheapTitles = ["Golf 3", "Volkswagen Polo", "Ford Escort", "Renault Megan", "Renault Clio",
-    "Moskwitsch", "Citroen C2", "Audi A3", "BMW M3", "Peugeot 203", "Opel Astra", "Mercedes 190"];
-    var cheapPrices = ["2.000лв", "2.000лв", "2.000лв", "2.000лв", "2.000лв", "2.000лв", "2.000лв", "2.000лв", "2.000лв", "2.000лв", "2.000лв", "2.000лв"];
-    
-luxury.onclick = function() {reset(), luxuryView()};
+	let cars = [
+	{
+		name: 'Mercedes Benz S Class', description: 'The classic luxury car',
+		price: 214300,
+		type: "lux"
+	},
+	{
+		name: 'Range Rover',
+		description: 'The classic luxury car',
+		price: 309900,
+		type: "lux"
+	},
+	{
+		name: 'Jaguar I-Pace',
+		description: 'The classic luxury car',
+		price: 121300,
+		type: "lux"
+	},
+	{
+		name: 'Tesla Model S',
+		description: 'The best electric luxury car',
+		price: 156600,
+		type: "lux"
+	},
+	{
+		name: 'Audi A8',
+		description: 'The classic luxury car',
+		price: 148000,
+		type: "lux"
+	},
+	{
+		name: 'BMW 7 Series',
+		description: 'The classic luxury car',
+		price: 146000,
+		type: "lux"
+	},
+	{
+		name: 'Jaguar XJ',
+		description: 'The classic luxury car',
+		price: 133900,
+		type: "lux"
+	},
+	{
+		name: 'BMW 6 Series Gran Turismo',
+		description: 'The classic luxury car',
+		price: 122700,
+		type: "lux"
+	},
+	{
+		name: 'Lexus LS',
+		description: 'The classic luxury car',
+		price: 131500,
+		type: "lux"
+	},
+	{
+		name: 'Maserati Quattroporte',
+		description: 'The classic luxury car',
+		price: 202000,
+		type: "lux"
+	},
+	{
+		name: 'Porsche Panamera',
+		description: 'The classic luxury car',
+		price: 181600,
+		type: "lux"
+	},
+	{
+		name: 'Lamborghini Aventador',
+		description: 'The classic luxury car',
+		price: 687400,
+		type: "lux"
+	},
+	{
+		name: 'Mini Cooper',
+		description: 'The classic economy car',
+		price: 55500,
+		type: "eco"
+	},
+	{
+		name: 'Honda Civic Hatchback',
+		description: 'The classic economy car',
+		price: 43000,
+		type: "eco"
+	},
+	{
+		name: 'Honda Accord',
+		description: 'The classic economy car',
+		price: 41400,
+		type: "eco"
+	},
+	{
+		name: 'Volkswagen Up!',
+		description: 'The classic economy car',
+		price: 9800,
+		type: "eco"
+	},
+	{
+		name: 'Ford Fiesta',
+		description: 'The classic economy car',
+		price: 24900,
+		type: "eco"
+	},
+	{
+		name: 'Kia Soul',
+		description: 'The classic economy car',
+		price: 28800,
+		type: "eco"
+	},
+	{
+		name: 'Chevrolet Spark',
+		description: 'The classic economy car',
+		price: 23100,
+		type: "eco"
+	},
+	{
+		name: 'Chevrolet Sonic',
+		description: 'The classic economy car',
+		price: 31500,
+		type: "eco"
+	},
+	{
+		name: 'Volkswagen Golf',
+		description: 'The classic economy car',
+		price: 38000,
+		type: "eco"
+	},
+	{
+		name: 'Hyundai Veloster',
+		description: 'The classic economy car',
+		price: 51700,
+		type: "eco"
+	},
+	{
+		name: 'Fiat 500',
+		description: 'The classic economy car',
+		price: 35800,
+		type: "eco"
+	},
+	{
+		name: 'BMW i3',
+		description: 'The classic economy car',
+		price: 83200,
+		type: "eco"
+	},
+	{
+		name: 'Golf 3',
+		description: 'The classic cheap car',
+		price: 3600,
+		type: "cheap"
+	},
+	{
+		name: 'Volkswagen Polo',
+		description: 'The classic cheap car',
+		price: 4000,
+		type: "cheap"
+	},
+	{
+		name: 'Ford Escort',
+		description: 'The classic cheap car',
+		price: 6800,
+		type: "cheap"
+	},
+	{
+		name: 'Renault Megan',
+		description: 'The classic cheap car',
+		price: 4200,
+		type: "cheap"
+	},
+	{
+		name: 'Renault Clio',
+		description: 'The classic cheap car',
+		price: 7000,
+		type: "cheap"
+	},
+	{
+		name: 'Moskwitsch',
+		description: 'The classic cheap car',
+		price: 1400,
+		type: "cheap"
+	},
+	{
+		name: 'Citroen C2',
+		description: 'The classic cheap car',
+		price: 3600,
+		type: "cheap"
+	},
+	{
+		name: 'Audi A3',
+		description: 'The classic cheap car',
+		price: 3300,
+		type: "cheap"
+	},
+	{
+		name: 'BMW M3',
+		description: 'The classic cheap car',
+		price: 12000,
+		type: "cheap"
+	},
+	{
+		name: 'Peugeot 206',
+		description: 'The classic cheap car',
+		price: 3000,
+		type: "cheap"
+	},
+	{
+		name: 'Opel Astra',
+		description: 'The classic cheap car',
+		price: 5700,
+		type: "cheap"
+	},
+	{
+		name: 'Mercedes 190',
+		description: 'The classic cheap car',
+		price: 3300,
+		type: "cheap"
+	}			]
+luxury.onclick = function() {disappear(), luxuryView()};
 function luxuryView() {
     luxury.className = "list-group-item-selected";         
     economy.className = "list-group-item";
     cheap.className = "list-group-item";
     
+    let car = cars.filter(c => c.type == "lux");
     for(i=0;i<12;i++)
     {
-    document.getElementsByClassName("card-text")[i].innerHTML = luxuryDescriptions[i];
-    document.getElementsByClassName("card-title-text")[i].innerHTML = luxuryTitles[i];
+    document.getElementsByClassName("card-text")[i].innerHTML = car[i].description;
+    document.getElementsByClassName("card-title-text")[i].innerHTML = car[i].name;
     document.getElementsByClassName('card-img-top')[i].src="Images/Content/Luxury/" + [i+1] + ".jpg";
-    document.getElementsByClassName("card-price")[i].innerHTML = luxuryPrices[i];
+    document.getElementsByClassName("card-price")[i].innerHTML = car[i].price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ", ") + " лв";
     }                           
 }
-economy.onclick = function() {reset(), economyView()};
+
+economy.onclick = function() {disappear(), economyView()};
 function economyView() {      
     luxury.className = "list-group-item";         
     economy.className = "list-group-item-selected";
     cheap.className = "list-group-item";
-    
+    let car = cars.filter(c => c.type == "eco");
     for(i=0;i<12;i++)
     {
-    document.getElementsByClassName("card-text")[i].innerHTML = economyDescriptions[i];
-    document.getElementsByClassName("card-title-text")[i].innerHTML = economyTitles[i];
+    document.getElementsByClassName("card-text")[i].innerHTML = car[i].description;
+    document.getElementsByClassName("card-title-text")[i].innerHTML = car[i].name;
     document.getElementsByClassName('card-img-top')[i].src="Images/Content/Economy/" + [i+1] + ".jpg";
-    document.getElementsByClassName("card-price")[i].innerHTML = economyPrices[i];
+    document.getElementsByClassName("card-price")[i].innerHTML = car[i].price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ", ") + " лв";
     }                           
 }
-cheap.onclick = function() {reset(), cheapView()};
+
+cheap.onclick = function() {disappear(), cheapView()};
 function cheapView() {         
     luxury.className = "list-group-item";         
     economy.className = "list-group-item";
     cheap.className = "list-group-item-selected";
     
+	let car = cars.filter(c => c.type == "cheap");
     for(i=0;i<12;i++)
     {
-    document.getElementsByClassName("card-text")[i].innerHTML = cheapDescriptions[i];
-    document.getElementsByClassName("card-title-text")[i].innerHTML = cheapTitles[i];
+    document.getElementsByClassName("card-text")[i].innerHTML = car[i].description;
+    document.getElementsByClassName("card-title-text")[i].innerHTML = car[i].name;
     document.getElementsByClassName('card-img-top')[i].src="Images/Content/Cheap/" + [i+1] + ".jpg";
-    document.getElementsByClassName("card-price")[i].innerHTML = cheapPrices[i];
+    document.getElementsByClassName("card-price")[i].innerHTML = car[i].price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ", ") + " лв";
     }                           
 }
+
+// Randomized main page content
+
 function randomizeContent()
 {
-    var descriptions = [].concat.apply([], [luxuryDescriptions, economyDescriptions,cheapDescriptions]);
-    var titles = [].concat.apply([], [luxuryTitles, economyTitles,cheapTitles]);
-    var prices = [].concat.apply([], [luxuryPrices, economyPrices,cheapPrices]);
-
     var arr = [];
     while(arr.length < 6){
         var r = Math.floor(Math.random()*36) + 1;
@@ -80,10 +284,32 @@ function randomizeContent()
         var str = "Images/Content/Economy/" + (arr[i]-12) + ".jpg";
     if(arr[i] <= 36 && arr[i] > 24)
         var str = "Images/Content/Cheap/" + (arr[i]-24) + ".jpg";
-    
-    document.getElementsByClassName("card-text")[i].innerHTML = descriptions[arr[i]-1];
-    document.getElementsByClassName("card-title-text")[i].innerHTML = titles[arr[i]-1];
+	
+    document.getElementsByClassName("card-text")[i].innerHTML = cars[arr[i]-1].description;
+    document.getElementsByClassName("card-title-text")[i].innerHTML = cars[arr[i]-1].name;
     document.getElementsByClassName('card-img-top')[i].src=str;
-    document.getElementsByClassName("card-price")[i].innerHTML = prices[arr[i]-1];
+    document.getElementsByClassName("card-price")[i].innerHTML = cars[arr[i]-1].price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ", ") + " лв";
     }
 }
+// Show more menu
+		document.getElementById("show-more").onclick = function() {showMore()};
+
+        function showMore() {
+        var moreItems = document.getElementsByClassName("mb-4_more");
+        var btn = document.getElementById("show-more");
+        btn.style.display = "none";
+        for(i=0;i<6;i++)
+        {
+          moreItems[i].style.display = "inline";
+		}
+		
+        }
+        function disappear() {
+        var moreItems = document.getElementsByClassName("mb-4_more");
+        var btn = document.getElementById("show-more");
+        btn.style.display = "inline";
+        for(i=0;i<6;i++)
+        {
+          moreItems[i].style.display = "none";
+        }
+        }
